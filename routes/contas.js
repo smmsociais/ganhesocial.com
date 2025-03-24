59dfb04623789f00e55fb3e0e73d7bffdb18f385
renissontk <renissontk@gmail.com>
2025-03-24 15:38:28 -0300

    Update contas.js

--- routes/contas.js
+++ routes/contas.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Conta = require("../models/Conta"); // Importa o modelo de Conta
-const authMiddleware = require("./middlewares/auth");
+const authMiddleware = require("../middlewares/auth");
 
 const router = express.Router();
 
