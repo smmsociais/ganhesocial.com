81518360640e83cc49fdb42e6137978b924105be
renissontk <renissontk@gmail.com>
2025-03-24 01:20:13 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,7 +1,39 @@
+const connectDB = require("../utils/db");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 module.exports = async (req, res) => {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método não permitido" });
   }
 
-  res.json({ message: "Login funcionando!" });
+  await connectDB();
+
+  const { email, senha } = req.body;
+  if (!email || !senha) {
+    return res.status(400).json({ error: "E-mail e senha são obrigatórios!" });
+  }
+
+  try {
+    const usuario = await User.findOne({ email });
+    if (!usuario) {
+      return res.status(400).json({ error: "Usuário não encontrado!" });
+    }
+
+    const senhaCorreta = await bcrypt.compare(senha, usuario.senha);
+    if (!senhaCorreta) {
+      return res.status(400).json({ error: "Senha incorreta!" });
+    }
+
+    const token = jwt.sign(
+      { id: usuario._id, email: usuario.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "2h" }
+    );
+
+    res.json({ message: "Login bem-sucedido!", token });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao realizar login" });
+  }
 };
