8442c77836a57169d013bd14d16204d972766cdb
renissontk <renissontk@gmail.com>
2025-03-24 16:13:39 -0300

    Create auth.js

---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+// middlewares/auth.js
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.header("Authorization");
+    if (!token) {
+        return res.status(401).json({ error: "Acesso negado, token não encontrado." });
+    }
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(400).json({ error: "Token inválido." });
+    }
+};
+
+module.exports = authMiddleware;
