37d2af056ca86a0ef1f74a74f0475ea847d3b316
renisson <rennissonn@gmail.com>
2025-03-24 20:27:43 -0300

    Reinstalando dependências

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,28 +1,30 @@
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
+import { jwtVerify } from "jose";
+import dotenv from "dotenv";
 
 // Carregar variáveis do .env
 dotenv.config();
 
-module.exports = (req, res, next) => {
+const secretKey = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export default async (req, res, next) => {
     const token = req.headers.authorization;
-    console.log("Token recebido:", token); // <-- Adicione esta linha
+    console.log("Token recebido:", token);
 
     if (!token) {
         return res.status(401).json({ error: "Acesso negado, token não encontrado." });
     }
 
     try {
-        const tokenSemBearer = token.split(" ")[1]; // Remover "Bearer "
-        console.log("Token processado:", tokenSemBearer); // <-- Adicione esta linha
+        const tokenSemBearer = token.split(" ")[1];
+        console.log("Token processado:", tokenSemBearer);
 
-        const decoded = jwt.verify(tokenSemBearer, process.env.JWT_SECRET);
-        console.log("Token decodificado:", decoded); // <-- Adicione esta linha
+        const { payload } = await jwtVerify(tokenSemBearer, secretKey);
+        console.log("Token decodificado:", payload);
 
-        req.user = decoded;
+        req.user = payload;
         next();
     } catch (error) {
-        console.error("Erro ao verificar token:", error); // <-- Adicione esta linha
+        console.error("Erro ao verificar token:", error);
         return res.status(400).json({ error: "Token inválido." });
     }
 };
