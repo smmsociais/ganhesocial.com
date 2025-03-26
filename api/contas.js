e231221010b296b82d1d86d5e6e9d05f1b3aac52
renissontk <renissontk@gmail.com>
2025-03-26 13:09:04 -0300

    Update contas.js

--- api/contas.js
+++ api/contas.js
@@ -14,8 +14,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", buscarAcaoRouter);
-
 // Conexão com o MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Conectado ao MongoDB"))
