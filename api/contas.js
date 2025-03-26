1241bcafe8d788ffdef3ed9ca78bdbefa76410d8
renissontk <renissontk@gmail.com>
2025-03-25 23:42:21 -0300

    Update contas.js

--- api/contas.js
+++ api/contas.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import bcrypt from "bcryptjs";
 import axios from "axios";
+import buscarAcaoRouter from "./api/buscar_acao.js";
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api", buscarAcaoRouter);
 
 // Conexão com o MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
