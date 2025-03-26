6f91cee800e09f29eb84cee09efd452c746586f8
renisson <rennissonn@gmail.com>
2025-03-25 22:46:52 -0300

    Primeiro commit

--- api/vincular_conta.js
+++ api/vincular_conta.js
@@ -1,6 +1,6 @@
-const axios = require("axios");
+import axios from "axios";
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Método não permitido" });
     }
