9486cc9ab8aaf5c071edcbf1eb418ecd7bfa5810
renissontk <renissontk@gmail.com>
2025-03-26 02:13:41 -0300

    Update contas.js

--- api/contas.js
+++ api/contas.js
@@ -201,56 +201,6 @@ app.get("/api/todas-contas", async (req, res) => {
     }
 });
 
-// Rota para vincular conta
-app.post("/api/vincular_conta", authMiddleware, async (req, res) => {
-    const { nomeUsuario } = req.body; // Recebe o nome do usuário a partir da requisição
-
-    if (!nomeUsuario) {
-        return res.status(400).json({ error: "Nome de usuário é obrigatório." });
-    }
-
-    const url = `http://api.ganharnoinsta.com/bind_tk.php?token=afc012ec-a318-433d-b3c0-5bf07cd29430&sha1=e5990261605cd152f26c7919192d4cd6f6e22227&nome_usuario=${nomeUsuario}`;
-
-    try {
-        const response = await axios.get(url);
-        const data = response.data;
-
-        if (data.status === "success") {
-            res.json({ id_conta: data.id_conta });
-        } else {
-            res.status(400).json({ error: "Erro ao vincular conta." });
-        }
-    } catch (error) {
-        console.error("Erro ao vincular conta:", error);
-        res.status(500).json({ error: "Erro ao vincular conta." });
-    }
-});
-
-// Rota para buscar ação
-app.get("/api/buscar_acao", authMiddleware, async (req, res) => {
-    const { id_conta } = req.query; // Recebe o id_conta a partir da query
-
-    if (!id_conta) {
-        return res.status(400).json({ error: "ID da conta é obrigatório." });
-    }
-
-    const url = `https://api.ganharnoinsta.com/get_action.php?token=afc012ec-a318-433d-b3c0-5bf07cd29430&sha1=e5990261605cd152f26c7919192d4cd6f6e22227&id_conta=${id_conta}&is_tiktok=1&tipo=1`;
-
-    try {
-        const response = await axios.get(url);
-        const data = response.data;
-
-        if (data.status === "ENCONTRADA") {
-            res.json(data);
-        } else {
-            res.status(404).json({ error: "Nenhuma ação encontrada." });
-        }
-    } catch (error) {
-        console.error("Erro ao buscar ação:", error);
-        res.status(500).json({ error: "Erro ao buscar ação." });
-    }
-});
-
 // Iniciar o Servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
