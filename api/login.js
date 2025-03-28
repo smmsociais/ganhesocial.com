c343cecbb8e6fc96abd2eade8fecf7146e564675
renissontk <renissontk@gmail.com>
2025-03-28 15:06:41 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,5 +1,5 @@
 import connectDB from "./db.js";
-import User from "./User.js";
+import User from "./User.js";  // Apenas importe, não defina novamente
 import jwt from "jsonwebtoken";
 
 const handler = async (req, res) => {
