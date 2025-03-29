3b045bc7f761c16f99257f8c20204f4952327eda
renissontk <renissontk@gmail.com>
2025-03-29 00:52:51 -0300

    Update User.js

--- api/User.js
+++ api/User.js
@@ -2,20 +2,21 @@ import mongoose from 'mongoose';
 
 // Definição do schema para contas
 const ContaSchema = new mongoose.Schema({
-    nomeConta: { type: String, required: true, unique: true },  // Se quiser garantir que cada nomeConta seja único
-    id_conta: { type: String, required: false },  // Tornando id_conta opcional
+    nomeConta: { type: String, required: true, unique: true },
+    id_conta: { type: String, required: false },
     id_tiktok: { type: String },
     s: { type: String },
-    status: { type: String, default: "Pendente" },  // Valor padrão para status
+    status: { type: String, default: "Pendente" },
 });
 
 // Definição do schema para o usuário
 const UserSchema = new mongoose.Schema({
     nome_usuario: { type: String, required: true },
-    email: { type: String, required: true, unique: true },  // Email único
+    email: { type: String, required: true, unique: true },
     senha: { type: String, required: true },
     token: { type: String, required: true },
     contas: [ContaSchema],  // Subdocumento de contas
+    historico_acoes: [{ type: mongoose.Schema.Types.ObjectId, ref: "ActionHistory" }]  // Relacionamento com ActionHistory
 }, { collection: 'usuarios' });
 
 const User = mongoose.models.User || mongoose.model('User', UserSchema);
