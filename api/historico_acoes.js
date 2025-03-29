3e3e5ed65a73cfa9e509f6b381c689e201cc6381
renissontk <renissontk@gmail.com>
2025-03-29 15:50:35 -0300

    Update historico_acoes.js

--- api/historico_acoes.js
+++ api/historico_acoes.js
@@ -9,17 +9,16 @@ export default async function handler(req, res) {
     await connectDB();
 
     try {
-        // Buscar todas as ações e popular o usuário associado
-        const historico = await ActionHistory.find().populate("user", "nome nome_usuario");
+        // Buscar todas as ações e popular o nome do usuário associado
+        const historico = await ActionHistory.find().populate("user", "nome");
 
-        // Formatar os dados corretamente
+        // Enviar apenas os campos necessários
         const formattedData = historico.map(action => ({
-            nome_usuario: action.user?.nome_usuario || "Desconhecido", // Nome da ação
-            nome_cadastrado: action.user?.nome || "Desconhecido", // Nome real do usuário no sistema
+            nome_usuario: action.nome_usuario,  // Nome do usuário da ação
+            nome_cadastrado: action.user?.nome || "Desconhecido", // Nome do usuário cadastrado no sistema
             acao_validada: action.acao_validada,  
             valor_confirmacao: action.valor_confirmacao,  
-            data: action.data,
-            valor: action.valor,
+            data: action.data  
         }));
 
         res.status(200).json(formattedData);
