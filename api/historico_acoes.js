23c7eb1b8ce31fd05084c5d9c21419ff451541b0
renissontk <renissontk@gmail.com>
2025-04-18 18:57:37 -0300

    Update historico_acoes.js

--- api/historico_acoes.js
+++ api/historico_acoes.js
@@ -14,13 +14,14 @@ export default async function handler(req, res) {
 
         // Enviar apenas os campos necessários
 const formattedData = historico.map(action => ({
-    nome_usuario: action.user?.nome_usuario || "Desconhecido",
+    nome_usuario: action.nome_usuario,
+    nome_cadastrado: action.user?.nome || "Desconhecido",
     acao_validada: action.acao_validada,
     valor_confirmacao: action.valor_confirmacao,
     data: action.data,
     rede_social: action.rede_social || "TikTok",
     tipo: action.tipo || "Seguir",
-    url_dir: action.url_dir || null
+    url_dir: action.url_dir || null  // 🔹 Adiciona a URL da ação
 }));
 
         res.status(200).json(formattedData);
