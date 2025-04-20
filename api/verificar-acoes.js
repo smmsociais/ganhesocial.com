233672cab8ec5559ab21b0ad3155d61c20780186
Ganhe Social <contato@ganhesocial.com>
2025-04-19 23:00:23 -0300

    Update verificar-acoes.js

--- api/verificar-acoes.js
+++ api/verificar-acoes.js
@@ -1,4 +1,5 @@
-import { MongoClient, ObjectId } from "mongodb";
+import pkg from "mongodb";
+const { MongoClient, ObjectId } = pkg;
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const API_URL = "https://ganhesocial.com/api";
@@ -8,6 +9,12 @@ let db = null;
 
 export default async function handler(req, res) {
   try {
+    if (req.method !== "GET") {
+      return res.status(405).json({ error: "Método não permitido." });
+    }
+
+    console.log("Rota verificar-acoes chamada:", new Date().toISOString());
+
     if (!db) {
       await client.connect();
       db = client.db("ganhesocial");
