88b3db95f547b956e119c6e2d1f7185970be9c05
Ganhe Social <contato@ganhesocial.com>
2025-04-22 18:33:27 -0300

    Update redis.js

--- api/redis.js
+++ api/redis.js
@@ -1,22 +1,7 @@
-import Redis from 'ioredis';
 
-let redis;
+import { Redis } from "@upstash/redis";
 
-if (process.env.REDIS_URL) {
-  if (!global.redis) {
-    global.redis = new Redis(process.env.REDIS_URL, {
-      maxRetriesPerRequest: 5,  // Limitar o número de tentativas
-      connectTimeout: 10000,    // Timeout de conexão (10 segundos)
-      retryStrategy: (times) => {
-        // Controlar o intervalo entre as tentativas
-        return Math.min(times * 50, 2000); // Aumentar exponencialmente até 2 segundos
-      },
-    });
-  }
-  
-  redis = global.redis;
-} else {
-  throw new Error("REDIS_URL não configurada no ambiente");
-}
+// Ele lê automaticamente estas duas variáveis de ambiente:
+const redis = Redis.fromEnv();
 
 export default redis;
