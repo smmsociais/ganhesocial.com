4a4935ace184b935d62b1accbfa808a577ad28a0
Ganhe Social <contato@ganhesocial.com>
2025-04-22 18:22:14 -0300

    Update redis.js

--- api/redis.js
+++ api/redis.js
@@ -4,7 +4,14 @@ let redis;
 
 if (process.env.REDIS_URL) {
   if (!global.redis) {
-    global.redis = new Redis(process.env.REDIS_URL);
+    global.redis = new Redis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: 5,  // Limitar o número de tentativas
+      connectTimeout: 10000,    // Timeout de conexão (10 segundos)
+      retryStrategy: (times) => {
+        // Controlar o intervalo entre as tentativas
+        return Math.min(times * 50, 2000); // Aumentar exponencialmente até 2 segundos
+      },
+    });
   }
   
   redis = global.redis;
