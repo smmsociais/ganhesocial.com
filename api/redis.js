ad804945eaa7f9677ca5e20d221d83b81d12606c
Ganhe Social <contato@ganhesocial.com>
2025-04-22 18:18:34 -0300

    Update redis.js

--- api/redis.js
+++ api/redis.js
@@ -2,10 +2,14 @@ import Redis from 'ioredis';
 
 let redis;
 
-if (!global.redis) {
-  global.redis = new Redis(process.env.REDIS_URL);
+if (process.env.REDIS_URL) {
+  if (!global.redis) {
+    global.redis = new Redis(process.env.REDIS_URL);
+  }
+  
+  redis = global.redis;
+} else {
+  throw new Error("REDIS_URL não configurada no ambiente");
 }
-
-redis = global.redis;
 
 export default redis;
