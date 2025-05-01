762facfc617ff662e2dd611ccf0bf74a53fc4c30
Ganhe Social <contato@ganhesocial.com>
2025-05-01 00:13:39 -0300

    Update change-password.js

--- api/change-password.js
+++ api/change-password.js
@@ -25,7 +25,7 @@ const handler = async (req, res) => {
 
         if (!usuario) {
             console.log("❌ Token inválido ou usuário não encontrado!");
-            return res.status(401).json({ error: "Token inválido" });
+            return res.status(401).json({ error: "Link inválido ou expirado" });
         }
 
         // (Opcional) Validar se o token expirou
