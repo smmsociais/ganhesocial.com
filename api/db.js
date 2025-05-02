4898cf9958226b6461b971a8567b06da3031d4a5
Ganhe Social <contato@ganhesocial.com>
2025-05-02 19:06:52 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,15 +1,28 @@
-// api/db.js (só conexão)
+// api/db.js
 import mongoose from "mongoose";
+
 const URI = process.env.MONGODB_URI;
 if (!URI) throw new Error("MONGODB_URI não definida!");
 
-let isConnected = false;
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 export default async function connectDB() {
-  if (isConnected) return;
-  await mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  isConnected = true;
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+
+  cached.conn = await cached.promise;
   console.log("🟢 Conectado ao MongoDB via Mongoose");
+  return cached.conn;
 }
