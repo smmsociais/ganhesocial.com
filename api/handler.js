aa684782902e333b751824f43663f9d102c1486e
Ganhe Social <contato@ganhesocial.com>
2025-05-15 21:47:07 -0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -1026,7 +1026,42 @@ if (url.startsWith("api/user-info")) {
             console.error("Erro ao buscar dados do TikTok:", error);
             res.status(500).json({ error: "Erro ao buscar dados do TikTok." });
         }
-    }       
+    }
+    
+// Rota: /api/user-following
+if (url.startsWith("api/user-following")) { 
+        if (req.method !== "GET") {
+            return res.status(405).json({ error: "Método não permitido." });
+        }
+    
+        const { userId } = req.query;
+    
+        if (!userId) {
+            return res.status(400).json({ error: "Parâmetro 'userId' é obrigatório." });
+        }
+    
+        const userFollowingUrl = `https://tiktok-scraper7.p.rapidapi.com/user/following?user_id=${userId}&count=200&time=0`;
+    
+        try {
+            const response = await axios.get(userFollowingUrl, {
+                headers: {
+                    "x-rapidapi-key":  process.env.rapidapi_key,
+                    "x-rapidapi-host": "tiktok-scraper7.p.rapidapi.com"
+                }
+            });
+    
+            const data = response.data;
+    
+            if (!data || Object.keys(data).length === 0) {
+                return res.status(404).json({ error: "Nenhuma informação de seguidores encontrada." });
+            }
+    
+            res.json(data);
+        } catch (error) {
+            console.error("Erro ao buscar seguidores:", error);
+            res.status(500).json({ error: "Erro ao buscar seguidores." });
+        }
+    }    
 
     // Rota não encontrada
     return res.status(404).json({ error: "Rota não encontrada." });
