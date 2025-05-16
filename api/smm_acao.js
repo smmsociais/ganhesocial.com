696c0ed01d04294bbce7e393fb81b913f92390de
Ganhe Social <contato@ganhesocial.com>
2025-05-15 22:57:36 -0300

    Update smm_acao.js

--- api/smm_acao.js
+++ api/smm_acao.js
@@ -2,7 +2,7 @@ import connectDB from './db.js';
 import { ActionHistory } from './User.js';
 
 const handler = async (req, res) => {
-    if (req.method !== "POST") {
+    if (req.method !== "GET") {
         return res.status(405).json({ error: "Método não permitido" });
     }
 
