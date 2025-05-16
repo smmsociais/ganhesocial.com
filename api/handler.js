ff25abb0872beeb12d9fd309d0f88ac6a493ef4a
Ganhe Social <contato@ganhesocial.com>
2025-05-15 21:12:26 -0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -717,6 +717,60 @@ if (url.startsWith("/api/get_saldo")) {
     }
 }
 
+// Rota: /api/login
+if (url.startsWith("/api/login")) {
+        if (req.method !== "POST") {
+            return res.status(405).json({ error: "Método não permitido" });
+        }
+    
+        try {
+            console.time("⏱️ Tempo total de login");
+            await connectDB();
+            console.timeLog("⏱️ Tempo total de login", "✔️ Conectado ao MongoDB");
+    
+            const { email, senha } = req.body;
+    
+            if (!email || !senha) {
+                return res.status(400).json({ error: "E-mail e senha são obrigatórios!" });
+            }
+    
+            console.log("🔍 Buscando usuário no banco de dados...");
+            const usuario = await User.findOne({ email });
+            console.timeLog("⏱️ Tempo total de login", "✔️ Usuário buscado");
+    
+            if (!usuario) {
+                console.log("🔴 Usuário não encontrado!");
+                return res.status(400).json({ error: "Usuário não encontrado!" });
+            }
+    
+            if (senha !== usuario.senha) {
+                console.log("🔴 Senha incorreta!");
+                return res.status(400).json({ error: "Senha incorreta!" });
+            }
+    
+            let token = usuario.token;
+            if (!token) {
+                token = jwt.sign({ id: usuario._id }, process.env.JWT_SECRET);
+                usuario.token = token;
+                await usuario.save({ validateBeforeSave: false });
+                console.timeLog("⏱️ Tempo total de login", "✔️ Token gerado e salvo");
+    
+                console.log("🟢 Novo token gerado e salvo.");
+            } else {
+                console.log("🟢 Token já existente mantido.");
+            }
+            console.timeEnd("⏱️ Tempo total de login");
+    
+            console.log("🔹 Token gerado para usuário:", token);
+            return res.json({ message: "Login bem-sucedido!", token });
+    
+        } catch (error) {
+            console.error("❌ Erro ao realizar login:", error);
+            return res.status(500).json({ error: "Erro ao realizar login" });
+        }
+        
+    };
+
     // Rota não encontrada
     return res.status(404).json({ error: "Rota não encontrada." });
 }
