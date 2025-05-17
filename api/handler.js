4e8299829fb34a45630689e5bb0405e720d76777
Ganhe Social <contato@ganhesocial.com>
2025-05-16 21:22:37 -0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -1068,7 +1068,7 @@ if (dadosSMM.status !== 'ENCONTRADA' || dadosSMM._id !== id_pedido) {
 // Verifica quantas ações (exceto recusadas) já existem para o pedido
 const acoesTotais = await ActionHistory.countDocuments({
   id_pedido,
-  acao_validada: { $ne: "false" }
+  acao_validada: { $in: [null, "true"] }
 });
 
 const limiteQuantidade = parseInt(dadosSMM.quantidade, 10) || 0;
