b7ff27b6c87e91142f74b488e1ee66db8fdb38e7
Ganhe Social <contato@ganhesocial.com>
2025-05-17 23:30:46 -0300

    Update ActionHistory.js

--- api/ActionHistory.js
+++ api/ActionHistory.js
@@ -4,11 +4,12 @@ const actionHistorySchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
   token: { type: String, required: false },
   nome_usuario: { type: String, required: true },
-  id_pedido: { type: String, required: false }, // ALTERADO para string
+  id_pedido: { type: String, required: false },
   id_conta: { type: String, required: false },
   url_dir: { type: String, required: true },
   tipo_acao: { type: String, required: true },
   quantidade_pontos: { type: Number, required: true },
+  quantidade: { type: Number, required: true }, // ✅ Adicionado
   tipo: { type: String, default: "Seguir" },
   rede_social: { type: String, default: "TikTok" },
   valor_confirmacao: { type: String, required: true },
