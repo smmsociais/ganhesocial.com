3a83015f1d11960cf6c28a78317ae68cd328ab00
Ganhe Social <contato@ganhesocial.com>
2025-05-21 21:39:45 -0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -1057,7 +1057,6 @@ const pedidos = await Pedido.find({
           nome_usuario: nomeUsuario,
           tipo_acao: "seguir",
           valor: valorFinal,
-          id_perfil: pedido._id.toString(),
           id_pedido: pedido._id.toString()
         },
         { upsert: true, new: true }
@@ -1070,7 +1069,6 @@ const pedidos = await Pedido.find({
         id_tiktok,
         id_action: idPedidoModificado,
         url: pedido.link,
-        id_perfil: pedido._id,
         nome_usuario: nomeUsuario,
         tipo_acao: "seguir",
         valor: valorFinal
@@ -1106,7 +1104,6 @@ await TemporaryAction.findOneAndUpdate(
     nome_usuario: nomeUsuario,
     tipo_acao: "seguir",
     valor: valorFinal,
-    id_perfil: pedido.id_conta, // ✅ Correção aqui
     id_pedido: pedido._id.toString()
   },
   { upsert: true, new: true }
@@ -1119,7 +1116,6 @@ await TemporaryAction.findOneAndUpdate(
     id_tiktok,
     id_action: idPedidoOriginal,
     url: data.url_dir,
-    id_perfil: data.id_alvo,
     nome_usuario: data.nome_usuario,
     tipo_acao: data.tipo_acao,
     valor: valorFinal
