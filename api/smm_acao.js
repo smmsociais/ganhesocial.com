da8104da8b75ea84364cc31a921b1b89d00cf289
Ganhe Social <contato@ganhesocial.com>
2025-05-28 20:18:21 -0300

    Update smm_acao.js

--- api/smm_acao.js
+++ api/smm_acao.js
@@ -77,7 +77,7 @@ if (!pedidoExistente) {
     nome: `Ação ${tipo_acao} - ${nome_usuario}`,
     valor: val,
     quantidade: qtd,
-    quantidadeExecutada: 0,
+    validadas: 0,
     link: url_dir,
     status: "pendente",
     dataCriacao: new Date(),
