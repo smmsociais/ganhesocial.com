80bc15cf1803ebc60c98d61491cc1b949c334d8f
SMM <contato@ganhesocial.com>
2025-06-12 23:31:36 -0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -740,40 +740,6 @@ if (url.startsWith("/api/recover-password")) {
         }
     };
     
-// Rota: /api/mailer
-if (url.startsWith("/api/mailer")) {
-
-      const transporter = nodemailer.createTransport({
-        host: 'smtpout.secureserver.net',
-        port: 465,
-        secure: true, // Porta 465 exige SSL
-        auth: {
-          user: 'contato@ganhesocial.com',
-          pass: 'reno4769!', // sua senha real
-        },
-      });
-    
-      const mailOptions = {
-        from: '"GanheSocial" <contato@ganhesocial.com>',
-        to: email,
-        subject: 'Recuperação de Senha',
-        html: `
-          <p>Você solicitou a recuperação de senha.</p>
-          <p>Clique no link abaixo para redefinir sua senha:</p>
-          <p><a href="${link}">${link}</a></p>
-          <p>Se você não solicitou essa recuperação, ignore este email.</p>
-        `,
-      };
-    
-      try {
-        await transporter.sendMail(mailOptions);
-        console.log(`Link de recuperação enviado para ${email}`);
-      } catch (error) {
-        console.error('Erro ao enviar email:', error);
-        throw new Error('Erro ao enviar email de recuperação');
-      }
-    }
-    
 // Rota: /api/registrar_acao_pendente
 if (url.startsWith("/api/registrar_acao_pendente")) {
   if (req.method !== "POST") {
