9511349e59ed005c464e240acacc43f2fdc619f8
SMM <contato@ganhesocial.com>
2025-10-02 23:46:34 -0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -1438,7 +1438,7 @@ if (url.startsWith("/api/withdraw")) {
         valor: amount,
         chave_pix: pixKey,
         tipo_chave: keyType,
-        status: "pending",
+        status: "PENDING",
         data: new Date(),
         asaasId: null,
         externalReference,
