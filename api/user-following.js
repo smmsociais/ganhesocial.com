15505e3704a56f18eeb80489977e6ec893a1978a
SMM <contato@ganhesocial.com>
2025-10-15 03:21:37 -0300

    Update user-following.js

--- api/user-following.js
+++ api/user-following.js
@@ -11,16 +11,20 @@ export default async function handler(req, res) {
             return res.status(400).json({ error: "Parâmetro 'userId' é obrigatório." });
         }
     
-        const userFollowingUrl = `https://tiktok-scraper7.p.rapidapi.com/user/following?user_id=${userId}&count=200&time=0`;
+        const userFollowingUrl = `https://cybrix-bytedance1.p.rapidapi.com/scraping/user/followings`;
     
         try {
             const response = await axios.get(userFollowingUrl, {
-                headers: {
-                    "x-rapidapi-key":  process.env.rapidapi_key,
-                    "x-rapidapi-host": "tiktok-scraper7.p.rapidapi.com"
+  headers: {
+    'x-rapidapi-key':  process.env.rapidapi_key,
+    'x-rapidapi-host': 'cybrix-bytedance1.p.rapidapi.com',
+    'Content-Type': 'application/json'
+  },
+  data: {
+    username: userId
                 }
             });
-    
+            
             const data = response.data;
     
             if (!data || Object.keys(data).length === 0) {
