fac6b6592146fc83284b7b65a53466bca62edc22
SMM <contato@ganhesocial.com>
2025-10-17 03:14:03 -0300

    Update verificar-follows.js

--- api/verificar-follows.js
+++ api/verificar-follows.js
@@ -67,7 +67,7 @@ export default async function handler(req, res) {
       tipo: "seguir",
     })
       .sort({ data: 1 })
-      .limit(5)
+      .limit(3)
       .toArray();
 
     console.log(`📦 Encontradas ${acoes.length} ações pendentes.`);
