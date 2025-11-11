f84826e4a63617ed7fb73aecd95481b914bf434b
SMM <contato@ganhesocial.com>
2025-11-11 16:57:35 -0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -1826,7 +1826,7 @@ let ranking = ganhosPorUsuario
 // Completa com nomes fixos se necessário (com real_total numérico)
 const nomesFixos = [
   "Allef 🔥","🤪","melzinho_443","noname","Caioo ⚡",
-  "-","joaozinxx_","brunno777","-","KingdosMTD🥱🥱","kaduzinho",
+  "-","-","brunno777","-","KingdosMTD🥱🥱","kaduzinho",
   "Rei do ttk 👑","Deus🔥","Mago ✟","ldzz tiktok'uva🍇","unknown",
   "vitor das continhas","the best of TikTok","@sc_geladinhos_gourmet",
   "Lipe Rodagem Interna 😄","dequelbest 🧙","luiza","xxxxxxxxxx",
